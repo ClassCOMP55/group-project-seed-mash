75c50e23d3debf7cc7636a6df8a08792bedac6fd
Maxwell Palacios <m_palacios3@u.pacific.edu>
2026-03-20 09:40:36 -0700

    Merge branch 'main' of https://github.com/ClassCOMP55/group-project-seed-mash into main

--- src/StartScreen.java
+++ src/StartScreen.java
@@ -6,25 +6,32 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class StartScreen extends GraphicsProgram{
-	private int musicVol = 100;
-	private int sfxVol = 100;
+	MainApplication mainApp;
 	ArrayList<GObject> settingsMenu = new ArrayList<GObject>();
 	GImage closeButton;
 	public static final int WINDOW_WIDTH = 800;
 	public static final int WINDOW_HEIGHT = 600;
+	public static final int ScaleWidth = 300;
 	
+	public StartScreen() {
+		mainApp = new MainApplication();
+	}
 	
-	public int getMusicVol() {
-		return musicVol;
+	public StartScreen(MainApplication mainApp) {
+		this.mainApp = mainApp;
+	}
+	
+	public double getMusicVol() {
+		return mainApp.getMusicVol();
 	}
 	public void setMusicVol(int musicVol) {
-		this.musicVol = musicVol;
+		mainApp.setMusicVol(musicVol);
 	}
-	public int getSfxVol() {
-		return sfxVol;
+	public double getSfxVol() {
+		return mainApp.getSfxVol();
 	}
 	public void setSfxVol(int sfxVol) {
-		this.sfxVol = sfxVol;
+		mainApp.setSfxVol(sfxVol);
 	}
 	
 	public void init() {
@@ -58,6 +65,27 @@ public class StartScreen extends GraphicsProgram{
 		closeButton.scale(0.22);
 		settingsMenu.add(closeButton);
 		
+		GRect musicScale = new GRect(250, 250, ScaleWidth, 25);
+		musicScale.setFillColor(new Color(19, 117, 203));
+		musicScale.setFilled(true);
+		settingsMenu.add(musicScale);
+		
+		GLabel musicLabel = new GLabel("Music Volume", 325, 225);
+		musicLabel.scale(2);
+		settingsMenu.add(musicLabel);
+		
+		GRect sfxScale = new GRect(250, 350, ScaleWidth, 25);
+		sfxScale.setFillColor(new Color(19, 117, 203));
+		sfxScale.setFilled(true);
+		settingsMenu.add(sfxScale);
+		
+		GLabel sfxLabel = new GLabel("SFX Volume", 330, 325);
+		sfxLabel.scale(2);
+		settingsMenu.add(sfxLabel);
+		
+		GOval musicSet = new GOval((ScaleWidth/100)*getMusicVol(), 345, 35, 35);
+		settingsMenu.add(musicSet);
+		
 		for (GObject x : settingsMenu) {
 			add(x);
 		}
@@ -67,7 +95,10 @@ public class StartScreen extends GraphicsProgram{
 		for (GObject x : settingsMenu) {
 			remove(x);
 		}
+		settingsMenu.clear();
 	}
+	
+	
 	
 	@Override
 	public void mouseClicked(MouseEvent e) { //For close button
@@ -77,6 +108,8 @@ public class StartScreen extends GraphicsProgram{
 			}
 		}
 	}
+	
+	
 	
 	public static void main(String[] args) {
 		new StartScreen().start();
