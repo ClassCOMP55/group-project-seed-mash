95d41958498e65bf72179cf850feff06b5efe4db
AnhP <112260892+AnhP1205@users.noreply.github.com>
2026-03-20 05:08:05 -0700

    Make the background and title for selectlevelPane

--- src/LevelSelectPane.java
+++ src/LevelSelectPane.java
@@ -1,17 +1,37 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.MouseEvent;
-
 import acm.graphics.*;
 
 public class LevelSelectPane extends GraphicsPane{
+
+	
+	private static class LevelData {
+		String name;
+		int difficulty; 
+		int stars;
+		boolean completed;
+		Color color;
+		
+		LevelData(String name, int difficulty, int stars, Color color) {
+			this.name = name;
+			this.difficulty = difficulty;
+			this.stars = stars;
+			this.completed = false;
+			this.color = color;
+		}
+	}
+	
+	private LevelData[] levels;
+	
 	public LevelSelectPane(MainApplication mainScreen) {
 		this.mainScreen = mainScreen;
 	}
 	
 	@Override
 	public void showContent() {
-		addText();
-		addBackButton();
+		addBackground();
+		addTitle();
 	}
 
 	@Override
@@ -22,30 +42,23 @@ public class LevelSelectPane extends GraphicsPane{
 		contents.clear();
 	}
 	
-	private void addText() {
-		GLabel text = new GLabel("This is an example of a new screen with some description!", 100, 70);
-		text.setColor(Color.BLUE);
-		text.setFont("DialogInput-PLAIN-24");
-		text.setLocation((mainScreen.getWidth() - text.getWidth()) / 2, 70);
-		
-		contents.add(text);
-		mainScreen.add(text);
+	private void addBackground() {
+		GRect bg = new GRect(0, 0, mainScreen.getWidth(), mainScreen.getHeight());
+		bg.setFilled(true);
+		bg.setColor(new Color(0, 102, 204)); // Geometry Dash blue
+		contents.add(bg);
+		mainScreen.add(bg);
 	}
 	
-	private void addBackButton() {
-		GImage backButton = new GImage("back.jpg", 200, 400);
-		backButton.scale(0.3, 0.3);
-		backButton.setLocation((mainScreen.getWidth() - backButton.getWidth())/ 2, 400);
+	private void addTitle() {
+		GLabel title = new GLabel("SELECT LEVEL");
+		title.setFont(new Font("Arial", Font.BOLD, 36));
+		title.setColor(Color.WHITE);
+		title.setLocation((mainScreen.getWidth() - title.getWidth()) / 2, 60);
 		
-		contents.add(backButton);
-		mainScreen.add(backButton);
+		contents.add(title);
+		mainScreen.add(title);
 	}
 	
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		if (mainScreen.getElementAtLocation(e.getX(), e.getY()) == contents.get(1)) {
-			mainScreen.switchToStartScreen();
-		}
-	}
 
 }
