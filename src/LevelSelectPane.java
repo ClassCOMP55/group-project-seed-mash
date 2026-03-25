fc043046b5abc2f45ecec50752ccf9f332f36294
Maxwell Palacios <m_palacios3@u.pacific.edu>
2026-03-25 10:33:36 -0700

    Fixed level select

--- src/LevelSelectPane.java
+++ src/LevelSelectPane.java
@@ -23,7 +23,7 @@ public class LevelSelectPane extends GraphicsPane {
 	public void showContent() {
 		addBackground();
 		addTitle();
-		addPlayButton();
+		drawLevelInfo();
 		addBackButton();
 		addLeftArrow();
 		addRightArrow();
@@ -141,13 +141,13 @@ public class LevelSelectPane extends GraphicsPane {
 			System.out.println("Back button clicked!");
 			mainScreen.switchToStartScreen();
 		} else if (clicked == leftArrow) {
-            currentSelection = Math.floorMod(currentSelection-1,2);
+            //currentSelection = Math.floorMod(currentSelection-1,2);
             System.out.println("current selection " + currentSelection);
             incrementSelection(-1);
             drawLevelInfo();
 
 		} else if (clicked == rightArrow) {
-            currentSelection = Math.floorMod(currentSelection+1,2);
+            //currentSelection = Math.floorMod(currentSelection+1,2);
             System.out.println("current selection " + currentSelection);
             incrementSelection(1);
             drawLevelInfo();
