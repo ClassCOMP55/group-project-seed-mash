074d244cb45db19a814eb4c9607889ee376003f3
Maxwell Palacios <m_palacios3@u.pacific.edu>
2026-04-05 04:20:11 -0700

    Hard coded character class and added it as obstacle for display

--- src/level/Character.java
+++ src/level/Character.java
@@ -1,4 +1,6 @@
 package level;
+
+
 import acm.graphics.*;
 import java.util.*;
 import acm.program.*;
@@ -7,79 +9,113 @@ import acm.util.*;
 import java.awt.*;
 import java.awt.event.*;
 
-
 public class Character extends GraphicsProgram{
 	
 	GImage sprite = new GImage("Character Sprite (1).png", 0, 0);
-	GRect hitbox;
-	GRect bottomHitbox;
 	ObstacleType[][] geometry;
 	private final GameLevel[] levels = {GameLevel.TEST_LEVEL, GameLevel.TEST_LEVEL_2};
 	GameLevel level; // the one I'm using
+	GridPos pos;
+	int characterXPos;
+	int characterYPos;
 	
 	public Character(int levelNum) {
 		level = levels[levelNum];
+		geometry = level.getGeometry();
 	}
 	
 	public void run() {
 		addMouseListeners();
 		sprite.scale(0.2);
 		add(sprite);
-		
-		GImage theBlock = new GImage("obstacles/block.png", 50, 50);
-		//add(theBlock);
-		
-		updateHitbox();
-		checkHitboxes();
+		startChar();
+		jump();
+		drop();
+		level.printGeom();
 	}
 	
-	public void updateHitbox() {
-		hitbox = new GRect(sprite.getX(), sprite.getY(), sprite.getWidth(), sprite.getHeight()-10); // The -10 is so the character can walk on ground
-		bottomHitbox = new GRect(hitbox.getX(), hitbox.getY() + hitbox.getHeight(), hitbox.getWidth(), 10); // The 10 is the blank space
+	public void createCharacter() {
+		sprite.scale(0.2);
+		add(sprite);
 	}
 	
-	public void checkHitboxes() {
-		GImage block = new GImage("obstacles/block.png");
-		GImage platform = new GImage("obstacles/platform.png");
-		GImage spikeDown = new GImage("obstacles/spike_down.png");
-		GImage spikeUp = new GImage("obstacles/spike_up.png");
-		
-		//Checks the right side
-		for (int i = 0; i < hitbox.getHeight(); i++) {
-			GObject element = getElementAt(hitbox.getX() + hitbox.getWidth() + 1, hitbox.getY() + i);
-			if (element != null) {
-				die();
-			}
-		}
-		//Checks the top side
-		for (int i = 0; i < hitbox.getWidth(); i++) {
-			GObject element = getElementAt(hitbox.getX() + i, hitbox.getY() - 1);
-			if (element != null) {
-				die();
-			}
-		}
-	}
 	
 	public void die() {
 		System.out.println("Dead.");
 	}
 	
-	public boolean checkImages(GImage obstacle, GImage element) {
-		if (element == null) {
-			return false;
-		}
-		int[][] elementList = element.getPixelArray();
-		int[][] obstacleList = obstacle.getPixelArray();
-		
-		for (int j = 0; j < elementList[0].length; j++) {
-			for (int i = 0; i < elementList.length; i++) {
-				if (elementList[i][j] != obstacleList[i][j]) {
-					return false;
-				}
+	public void startChar() {
+		//Starts character on top of whatever the first block is
+		int startY = 0;
+		for (int i = 0; i < geometry.length; i++) {
+			if (geometry[geometry.length-1-i][0] != null) {
+				startY++;
 			}
 		}
-		return true;
+		
+		characterXPos = 0;
+		characterYPos = startY;
+		pos = new GridPos(characterXPos, characterYPos);
+		geometry[characterYPos][characterXPos] = ObstacleType.CHARACTER;
 	}
+	
+	public boolean somethingInFront() { //returns true if something is there
+		if (geometry[characterYPos][characterXPos+1] != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean somethingAbove() { //returns true if something is there
+		if (geometry[characterYPos+1][characterXPos] != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public boolean somethingBelow() { //returns true if something is there
+		if (geometry[characterYPos-1][characterXPos] != null) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public void moveForward() {
+		if (!somethingInFront()) {
+			geometry[characterYPos][characterXPos] = null;
+			characterXPos++;
+			geometry[characterYPos][characterXPos] = ObstacleType.CHARACTER;
+		} else {
+			die();
+		}
+	}
+	
+	public void jump() { //jumps three blocks up
+		for (int i = 0; i < 3; i++) {
+			if (!somethingAbove()) {
+				geometry[characterYPos][characterXPos] = null;
+				characterYPos++;
+				geometry[characterYPos][characterXPos] = ObstacleType.CHARACTER;
+			} else {
+				die();
+			}
+		}
+	}
+	
+	public void drop() { //drop character
+		while (!somethingBelow()) {
+			geometry[characterYPos][characterXPos] = null;
+			characterYPos--;
+			geometry[characterYPos][characterXPos] = ObstacleType.CHARACTER;
+		}
+		if (geometry[characterYPos-1][characterXPos].getCharRepresentation() == '^') {
+			die();
+		}
+	}
+	
 	public static void main(String[] args) {
 		new Character(0).start();
 	}
