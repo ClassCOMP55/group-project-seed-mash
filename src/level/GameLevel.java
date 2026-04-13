5c91674f471e511515c18f42100e905e5c63423e
AnhP <112260892+AnhP1205@users.noreply.github.com>
2026-04-13 10:11:47 -0700

    few change

--- src/level/GameLevel.java
+++ src/level/GameLevel.java
@@ -111,19 +111,7 @@ public class GameLevel {
             "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
     };
 
-    public  static final String[] FINAL_DESTI = {
-            "                                                                                                ",
-            "                                                                                                ",
-            "                                                                                                ",
-            "                                                                                                ",
-            "                                                                                                ",
-            "                                                       B                                        ",
-            "                                                  BBBBB                                         ",
-            "                                                                                                ",
-            "                                                                                                ",
-            "            ^                                                                                   ",
-            "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
-    };
+
     
     public static final GameLevel TEST_LEVEL = new GameLevel(
             "Test Level 1",
@@ -152,21 +140,7 @@ public class GameLevel {
             3,
             1
     );
-    
-    public static final GameLevel FINAL_DESTINATION = new GameLevel(
-    		"Final Destination",
-    		"final_destination",
-    		 new LevelStitcher.ColorScheme(
-                     new Color(255, 100, 10),
-                     Color.WHITE,
-                     new Color(50, 0, 0)
-             ),
-             60,
-             geomFromString(FINAL_DESTI),
-             3,
-             1
-    		
-    		);
+
 
     public int getDifficulty() {
         return difficulty;
@@ -247,4 +221,34 @@ public class GameLevel {
             3,
             0
     );
+    
+    public  static final String[] FINAL_DESTI = {
+            "                                                                                                ",
+            "                                                                                                ",
+            "                                                                                                ",
+            "                                                                                                ",
+            "                                                                                                ",
+            "                                                       B                                        ",
+            "                                                  BBBBB                                         ",
+            "                                                                                                ",
+            "                                                                                                ",
+            "            ^                                                                                   ",
+            "BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB",
+    };
+    
+    public static final GameLevel FINAL_DESTINATION = new GameLevel(
+    		"Final Destination",
+    		"final_destination",
+    		 new LevelStitcher.ColorScheme(
+                     new Color(255, 100, 10),
+                     Color.WHITE,
+                     new Color(50, 0, 0)
+             ),
+             60,
+             geomFromString(FINAL_DESTI),
+             3,
+             1
+    		
+    		);
+
 }
